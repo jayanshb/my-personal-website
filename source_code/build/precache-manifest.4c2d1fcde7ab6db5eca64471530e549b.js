be3ae8b4b72a7e5b703a312a476ac1c2e18d47a0
= <=>
2020-09-08 11:56:56 -0400

    added build

---
+++ source_code/build/precache-manifest.4c2d1fcde7ab6db5eca64471530e549b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d0fbcc10df99d13d792a2bac8224fad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e191399f5c7ebda0e9ab",
+    "url": "/static/css/2.3ea3a538.chunk.css"
+  },
+  {
+    "revision": "6d91d3a00b7682dd0bbc",
+    "url": "/static/css/main.1f161387.chunk.css"
+  },
+  {
+    "revision": "e191399f5c7ebda0e9ab",
+    "url": "/static/js/2.d1b0f69f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d1b0f69f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d91d3a00b7682dd0bbc",
+    "url": "/static/js/main.1e15e4e9.chunk.js"
+  },
+  {
+    "revision": "042a2e2bb04323af66fa",
+    "url": "/static/js/runtime-main.c9ea7154.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "04537726e12339cfc92680bab73c0c15",
+    "url": "/static/media/resume.04537726.pdf"
+  }
+]);
--- source_code/src/pages/Projects.jsx
+++ source_code/src/pages/Projects.jsx
@@ -3,7 +3,7 @@ import PCard from "../components/projectcard";
 import Sidebar from "../components/sidebar";
 import "../design/projects.css";
 
-import ScrollAnimation from 'react-animate-on-scroll';
+import ScrollAnimation from "react-animate-on-scroll";
 
 export default class Projects extends React.Component {
   render() {
@@ -11,80 +11,72 @@ export default class Projects extends React.Component {
       <div className="container-fluid">
         <div className="row">
           <Sidebar />
-          <div className="col-xl-9 whiteCarryover clearfix" style={{  }}>
+          <div className="col-xl-9 whiteCarryover clearfix" style={{}}>
             <div className="row">
               <div className="card-deck">
-              <div className="col-xl-3">
-              <ScrollAnimation animateIn="fadeIn" delay={1000}>
-                <div className="align-items-center">
-                  <div className="mt-3" id="qlearning">
-                    <PCard
-                      course="AI"
-                      code="Game playing bot"
-                      name=""
-                      positions="Machine Learning"
-                      duties="This project implements a Gaming Bot for the game Frozen Lake from OpenAI Gym, trained using Q Learning."
-                      page="qlearning"
-                      link="https://github.com/jayanshb/FrozenLakeGameQLearningAI"
-                      duration="March 2020"
-                    />
+                <div className="col-xl-3">
+                  <div className="align-items-center">
+                    <div className="mt-3" id="qlearning">
+                      <PCard
+                        course="AI"
+                        code="Game playing bot"
+                        name=""
+                        positions="Machine Learning"
+                        duties="This project implements a Gaming Bot for the game Frozen Lake from OpenAI Gym, trained using Q Learning."
+                        page="qlearning"
+                        link="https://github.com/jayanshb/FrozenLakeGameQLearningAI"
+                        duration="March 2020"
+                      />
+                    </div>
                   </div>
-                </div>
-                </ScrollAnimation>
                 </div>
                 <div className="col-xl-3">
-                <ScrollAnimation animateIn="fadeIn" delay={1500}>
-                <div className="" id="webdev">
-                  <div className="mt-3">
-                    <PCard
-                      course="Designing"
-                      code="my website"
-                      name=""
-                      positions="Web development"
-                      duties="This fully responsive portfolio website was designed using bootstrap and React.js"
-                      page="webdev"
-                      link="https://github.com/jayanshb/Personal-Website/tree/master/source_code"
-                      duration="May 2020"
-                    />
+                  <div className="" id="webdev">
+                    <div className="mt-3">
+                      <PCard
+                        course="Designing"
+                        code="my website"
+                        name=""
+                        positions="Web development"
+                        duties="This fully responsive portfolio website was designed using bootstrap and React.js"
+                        page="webdev"
+                        link="https://github.com/jayanshb/my-personal-website"
+                        duration="May 2020"
+                      />
+                    </div>
                   </div>
-                </div>
-                </ScrollAnimation>
                 </div>
                 <div className="col-xl-3">
-                <ScrollAnimation animateIn="fadeIn" delay={2000}>
-                <div className="" id="deeplearning">
-                  <div className="mt-3">
-                    <PCard
-                      course="AI"
-                      code="Algorithmic trading bot"
-                      name=""
-                      positions="Deep learning and data analysis"
-                      duties="This is an ongoing project for the development of an AI bot for technical trading. Till now, this project has implemented analyzed RSI, MACD, and Dual moving crossover indicator stratergies. Stock price prediction using LSTM has also been implemented. Currently, I am working on integrating Deep-Q-Learning in order to automate efficient trades"
-                      page="deeplearning"
-                      link="https://github.com/jayanshb/Quant-Trading"
-                      duration="May 2020 - present"
-                    />
+                  <div className="" id="deeplearning">
+                    <div className="mt-3">
+                      <PCard
+                        course="AI"
+                        code="Algorithmic trading bot"
+                        name=""
+                        positions="Deep learning and data analysis"
+                        duties="This is an ongoing project for the development of an AI bot for technical trading. Till now, this project has implemented analyzed RSI, MACD, and Dual moving crossover indicator stratergies. Stock price prediction using LSTM has also been implemented. Currently, I am working on integrating Deep-Q-Learning in order to automate efficient trades"
+                        page="deeplearning"
+                        link="https://github.com/jayanshb/Quant-Trading"
+                        duration="May 2020 - present"
+                      />
+                    </div>
                   </div>
-                </div>
-                </ScrollAnimation>
                 </div>
                 <div className="col-xl-3">
-                <ScrollAnimation animateIn="fadeIn" delay={2500}>
-                <div className="" id="datascience">
-                  <div className="mt-3">
-                    <PCard
-                      course="Data Science"
-                      code="Library"
-                      name=""
-                      positions="Functional Programming"
-                      duties="Open source data science libraries using OCaml to contribute to the lack of functionality on multidimensional arrays in OCaml. Consists of functionality such as manipulating CSV files, indexing & slicing data, and providing linear algebra functionalities on arrays."
-                      page="datascience"
-                      link="https://github.com/jayanshb/DataCaml"
-                      duration="May 2020 - present"
-                    />
+                  <div className="" id="datascience">
+                    <div className="mt-3">
+                      <PCard
+                        course="Data Science"
+                        code="Library"
+                        name=""
+                        positions="Functional Programming"
+                        duties="Open source data science libraries using OCaml to contribute to the lack of functionality on multidimensional arrays in OCaml. Consists of functionality such as manipulating CSV files, indexing & slicing data, and providing linear algebra functionalities on arrays."
+                        page="datascience"
+                        link="https://github.com/jayanshb/DataCaml"
+                        duration="May 2020 - present"
+                      />
+                    </div>
                   </div>
-                </div>
-                </ScrollAnimation>
                 </div>
               </div>
             </div>
